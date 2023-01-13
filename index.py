30b6d5c6190781cc6fc6973f1b5109494a70751a
Flag Hunter <32909577+FlagHunter01@users.noreply.github.com>
2023-01-13 14:03:18 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,36 +1,21 @@
 #!/usr/bin/python3.9
 
 import os
+import User
+import Content
+
+### Variables ###
 
 # Name of the connected user. "None" if none.
 username = os.environ.get('REMOTE_USER')
 
-### Functions ### 
+### Body ###
 
-# Returns the groups assigned to a name
-def Groups(name):
-    users = {
-        "None": ("everyone"),
-    }
+user_groups = User.Groups(username)
+content = Content.List(user_groups)
+for item in content:
+    print(item[0])
 
-    if users[name]:
-        return users[name]
-    else:
-        return users["None"]
-
-# Returns the groups a the user is part of
-def Contents(user_groups):
-    contents = [
-        ["Website", "https://frolov.eu", ("everyone"), False],
-    ]
-    allowed_content = list()
-    for content in contents:
-        for group in content[1]:
-            for user_group in user_groups:
-                if group == user_group:
-                    allowed_content.append(content)
-                    break
-
-
-
-
+print("Content-type: text/html\n\n")
+with open(file) as file:
+    print(file.read())
